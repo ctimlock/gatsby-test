14984a1b64c90a324df2e5704534580ff53748ee
Charlie <ctimlock@gmail.com>
2022-02-27 18:52:11 +1100

    Finished Gastby tute P.4

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import { Layout } from '../components'
 
-const Blog = () => {
+const Blog = ( {data} ) => {
   return (
     <Layout pageTitle='My Blog Posts'>
-        <p>My cool posts will go in here</p>
+        <ul>
+            {
+                data.allFile.nodes.map(node => (
+                    <li key={node.name}>
+                        {node.name}
+                    </li>
+                    )
+                )
+            }
+        </ul>
     </Layout>
   )
 }
+
+export const query = graphql`
+    query {
+        allFile {
+            nodes {
+                name
+            }
+        }
+    }
+  `
 
 export default Blog
