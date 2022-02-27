7d665818155a98ceca4f87ac6b16f69c477c3f69
Charlie <ctimlock@gmail.com>
2022-02-27 16:27:22 +1100

    pre-audit fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
     siteMetadata: {
         siteUrl: `https://www.yourdomain.tld`,
+        title: 'Lighthouse Sound Co.'
     },
     plugins: [
         "gatsby-plugin-image",
---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Layout } from '../components'
+
+const Blog = () => {
+  return (
+    <Layout pageTitle='My Blog Posts'>
+        <p>My cool posts will go in here</p>
+    </Layout>
+  )
+}
+
+export default Blog
